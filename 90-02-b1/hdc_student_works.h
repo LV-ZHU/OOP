07a432392768e8a1361cb33e98ded3de2cdbf7be
﻿LV-ZHU <212522225+LV-ZHU@users.noreply.github.com>
2026-03-14 14:24:44 +0800

    Add files via upload

---
+++ 90-02-b1/hdc_student_works.h
@@ -0,0 +1,10 @@
+/* REDACTED */
+#pragma once
+
+/* 数学曲线的声明 */
+void hdc_draw_Mathematical_curve_1234567();
+void hdc_draw_Mathematical_curve_STUDENT_ID();
+
+/* 卡通人物函数的声明 */
+void hdc_draw_cartoon_1234567(const int base_x, const int base_y); 
+void hdc_draw_cartoon_STUDENT_ID(const int base_x, const int base_y);
